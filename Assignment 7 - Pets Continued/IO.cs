a5feac16138b98cdfe0857a98698c9753b772de7
Tito <tito13kfm@gmail.com>
2022-03-31 13:31:59 -0400

    Add project files.

---
+++ Assignment 7 - Pets Continued/IO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment_7___Pets_Continued
+{
+    internal class IO
+    {
+        public static string Read(string prompt) { Console.Write(prompt + " "); return Console.ReadLine(); }
+
+        public static int ReadPosInt(string prompt) // Only accepts positive integers, and returns integer.
+        {
+            int integer = 0;
+            bool valid = false;
+            while (!valid | integer <= 0)
+            {
+                valid = int.TryParse(Read(prompt), out integer);
+                if (!valid | integer == 0) { Console.WriteLine("Please enter a valid positive integer"); }
+            }
+            return integer;
+        }
+        public static bool ReadYesNo(string prompt) // Accepts any string starting with y to be returned as true.
+        {
+            bool input = (Read(prompt)).ToLower().StartsWith("y");
+            return input;
+        }
+    }
+}
---
+++ Assignment 7 - Pets Continued/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_7___Pets_Continued
+{
+    internal class Pet
+    {
+        public string name, breed;
+        public int age;
+        public static int totalNumberOfPets = 0, sumOfAllPetAges = 0;
+        public static bool allFixed = true; // need to declare this true by default so any single false will make it false
+
+        public static void PrintPetStats()
+        {
+            Console.WriteLine("You have {0} pets", totalNumberOfPets);
+            Console.WriteLine("Their ages add up to {0}", sumOfAllPetAges);
+            double sum = (double)sumOfAllPetAges / (double)totalNumberOfPets; // figure out average age
+            Console.WriteLine("Which means their average age is approximately {0}", sum.ToString("0.00")); // format for 2 decimal points
+            Console.WriteLine((allFixed) ? "Thank you for helping to control the pet population" : "Help control the pet population, have your pets spayed or neutered"); //RIP Bob
+        }
+    }
+}
